daf6fee8da9886050bac06684d2d3e8f0c669f3a
Damian Mooyman <damian.mooyman@gmail.com>
2014-02-17 13:25:16 +1300

    API better filtering of eligible spam field candidates

--- code/EditableSpamProtectionField.php
+++ code/EditableSpamProtectionField.php
@@ -13,14 +13,28 @@ if(class_exists('EditableFormField')) {
 		private static $singular_name = 'Spam Protection Field';
 	
 		private static $plural_name = 'Spam Protection Fields';
+		
+		/**
+		 * Fields to include spam detection for
+		 *
+		 * @var array
+		 * @config
+		 */
+		private static $check_fields = array(
+			'EditableEmailField',
+			'EditableTextField',
+			'EditableNumericField'
+		);
 	
-		function getFormField() {
+		public function getFormField() {
+			
 			// Generate field from protector
 			$protector = SpamProtectorManager::instance();
 			if(!$protector) return false;
-			$field = $protector->getFormField($this->Name, $this->Title, null);
+			$field = $protector->getFormField($this->Name, $this->Title);
 			
-			// Extract saved field mappings and save in this field
+			// Extract saved field mappings and update this field. This method is substituted for the execution
+			// of SpamProtectorManager::update_form, since we can't call this on the form directly
 			$spamProtector = Config::inst()->get('SpamProtectorManager', 'spam_protector');
 			if($spamProtector) {
 				$mappableFields = Config::inst()->get($spamProtector, 'mappable_fields');
@@ -28,14 +42,13 @@ if(class_exists('EditableFormField')) {
 					
 					// Extract and assign mappings
 					$map = array();
-					foreach($this->getSiblingFields() as $otherField) {
-						$mapName = "Map-{$otherField->Name}";
-						$mapValue = $this->getSetting($mapName);
+					foreach($this->getDetectableFields() as $otherField) {
+						$mapSetting = "Map-{$otherField->Name}";
+						$mapValue = $this->getSetting($mapSetting);
 						if($mapValue && in_array($mapValue, $mappableFields)) {
 							$map[$otherField->Name] = $mapValue;
 						}
 					}
-					Debug::dump($map);
 					
 					$field->setFieldMapping($map);
 				}
@@ -44,11 +57,26 @@ if(class_exists('EditableFormField')) {
 			return $field;
 		}
 		
-		protected function getSiblingFields() {	
+		/**
+		 * Gets the list of all candidate spam detectable fields on this field's form
+		 * 
+		 * @return DataList
+		 */
+		protected function getDetectableFields() {
+			
+			// Get list of all configured classes available for spam detection
+			$types = Config::inst()->get('EditableSpamProtectionField', 'check_fields');
+			$typesInherit = array();
+			foreach($types as $type) {
+				$subTypes = ClassInfo::subclassesFor($type);
+				$typesInherit = array_merge($typesInherit, $subTypes);
+			}
+			
 			// Get anti-spam field candidates
 			return $this
 				->Parent()
 				->Fields() // Get sibling fields
+				->filter('ClassName', $typesInherit)
 				->exclude('ID', $this->ID)
 				->exclude('Title', ''); // Ignore this field and those without titles
 		}
@@ -68,13 +96,13 @@ if(class_exists('EditableFormField')) {
 					$mappableFieldMerged = array_combine($mappableFields, $mappableFields);
 					$mapGroup = new FieldGroup('Spam Field Mapping');
 					$mapGroup->setDescription('Select the form fields that correspond to any relevant spam protection identifiers');
-					foreach($this->getSiblingFields() as $otherField) {
-						$mapName = "Map-{$otherField->Name}";
+					foreach($this->getDetectableFields() as $otherField) {
+						$mapSetting = "Map-{$otherField->Name}";
 						$mapGroup->push(DropdownField::create(
-							$this->getSettingName($mapName),
+							$this->getSettingName($mapSetting),
 							$otherField->Title,
 							$mappableFieldMerged,
-							$this->getSetting($mapName)
+							$this->getSetting($mapSetting)
 						)->setEmptyString(''));
 					}
 					$fields->push($mapGroup);
